9159f4552115733d4a7231670b5a734868911da6
sumeeth07 <97532835+sumeeth07@users.noreply.github.com>
2024-10-25 10:28:35 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -33,12 +33,12 @@ resource "aws_security_group" "Project-SG" {
 resource "aws_instance" "web" {
   ami                    = "ami-001f2488b35ca8aad"
   instance_type          = "t2.large"
-  key_name               = "Kastro"
+  key_name               = "MyNewKey"
   vpc_security_group_ids = [aws_security_group.Project-SG.id]
   user_data              = templatefile("./resource.sh", {})
 
   tags = {
-    Name = "Kastro"
+    Name = "Sumeeth"
   }
   root_block_device {
     volume_size = 30
